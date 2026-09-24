109b9ce870e3b888be3288ca3f0a1ce48cbf0ed2
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-09-24 12:52:49 +0000

    blue screen of life

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
-// You will write all your code for this tutorial here!
+#include <bn_backdrop.h>
+#include <bn_color.h>
+#include <bn_core.h>
+
+
+int main() {
+    bn::core::init();
+
+    bn::backdrop::set_color(bn::color(20, 20, 31));
+
+    while(true) {
+        bn::core::update();
+    }
+
+
+}
